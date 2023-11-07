b8536023ef2467c6c099c34070ad8bc67db8a160
killian-vincent <killian.vincent@livecampus.tech>
2023-11-07 15:57:45 +0100

    Add registration file

---
+++ registration.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+</head>
+<body>
+    
+</body>
+</html>
+<body>
+    <h2>Inscription</h2>
+    <form method="post" action="#">
+        <label for="email">Adresse e-mail:</label>
+        <input type="email" name="email" required><br>
+
+        <label for="password">Mot de passe:</label>
+        <input type="password" name="password" required><br>
+
+        <input type="submit" name="register" value="S'inscrire">
+    </form>
+</body>
+</html>
