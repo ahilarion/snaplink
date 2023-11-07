029d065693d4a9627ae5cc5d3c642e649d2b6fed
killian-vincent <killian.vincent@livecampus.tech>
2023-11-07 16:31:45 +0100

    Add login file

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+</head>
+<body>
+    
+</body>
+</html>
+<body>
+    <h2>Connexion</h2>
+    <form method="post" action="#">
+        <label for="email">Adresse e-mail:</label>
+        <input type="email" name="email" required><br>
+
+        <label for="password">Mot de passe:</label>
+        <input type="password" name="password" required><br>
+
+        <input type="submit" name="login" value="Se connecter">
+    </form>
+</body>
+</html>
