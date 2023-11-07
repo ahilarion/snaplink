5893bd47e3ca64449f968611bd80073a45f1d6ea
killian-vincent <killian.vincent@livecampus.tech>
2023-11-07 16:25:16 +0100

    Add check user and hashed password in registration file

--- registration.php
+++ registration.php
@@ -1,3 +1,33 @@
+<?php
+if (isset($_POST['register'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $db = new PDO("mysql:host=localhost:8889;dbname=shortcut-url", "root", "root");
+
+    $checkQuery = "SELECT COUNT(*) FROM users WHERE email = ?";
+    $checkStmt = $db->prepare($checkQuery);
+    $checkStmt->execute([$email]);
+    $userExists = $checkStmt->fetchColumn();
+
+    if ($userExists == 0) {
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+        $insertQuery = "INSERT INTO users (email, password) VALUES (?, ?)";
+        $stmt = $db->prepare($insertQuery);
+        
+        if ($stmt->execute([$email, $hashedPassword])) {
+            header("Location: index.php");
+            exit();
+        } else {
+            
+        }
+    } else {
+
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,9 +39,8 @@
     
 </body>
 </html>
-<body>
     <h2>Inscription</h2>
-    <form method="post" action="#">
+    <form method="post" action="">
         <label for="email">Adresse e-mail:</label>
         <input type="email" name="email" required><br>
 
