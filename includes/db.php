b09fe1acfd8c815fa31032261f4bc1ebead9c99a
Alan <alan.hilarion@proton.me>
2023-11-07 16:14:21 +0100

    Add : DB class

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,37 @@
 <?php
 
+use Dotenv\Dotenv;
 class DB {
+    private mysqli $connection;
 
+    public function __construct() {
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+        $dotenv->load();
+
+        $dbHost = $_ENV['DB_HOSTNAME'];
+        $dbUsername = $_ENV['DB_USERNAME'];
+        $dbPassword = $_ENV['DB_PASSWORD'];
+        $dbName = $_ENV['DB_DATABASE'];
+
+        $this->connection = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+        if ($this->connection->connect_error) {
+            die("Connection failed: " . $this->connection->connect_error);
+        }
+    }
+
+    public function query($sql) {
+        $result = $this->connection->query($sql);
+
+        if (!$result) {
+            die("Query failed: " . $this->connection->error);
+        }
+
+        return $result;
+    }
+
+    public function close(): void
+    {
+        $this->connection->close();
+    }
 }
