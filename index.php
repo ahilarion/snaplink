9fd2bca9d1f8100084a0f301f8c6d60f539f59d4
Nelo07o <90777157+Nelo07o@users.noreply.github.com>
2023-11-08 18:43:48 +0100

    add style structure

--- index.php
+++ index.php
@@ -34,9 +34,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="assets/css/styles.css">
-    <script defer src="assets/js/script.js"></script>
-    <title>URL Shorter</title>
+    <link rel="stylesheet" href="dist/css/style.css">
+    <script defer src="dist/js/script.js"></script>
+    <title>SnapLink</title>
 </head>
 <body>
     <nav>
