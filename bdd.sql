751f24be5cd30c189084660ccdf95a097641af90
Alan <alan.hilarion@proton.me>
2023-11-10 15:16:06 +0100

    Update : bdd.sql

--- bdd.sql
+++ bdd.sql
@@ -1,144 +1,25 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Hôte : localhost:8889
--- Généré le : jeu. 09 nov. 2023 à 13:25
--- Version du serveur : 5.7.39
--- Version de PHP : 8.2.0
+-- Création de la table 'users'
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT,
+    uuid CHAR(36) DEFAULT (UUID()),
+    username VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255),
+    PRIMARY KEY (id)
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données : `shorter-url`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `files`
---
-
-CREATE TABLE `files` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `file_name` varchar(255) NOT NULL,
-  `long_url` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `urls`
---
-
-CREATE TABLE `urls` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `long_url` varchar(255) NOT NULL,
-  `short_url` varchar(255) NOT NULL,
-  `clic_count` int(11) DEFAULT '0',
-  `disabled` tinyint(1) DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `urls`
---
-
-INSERT INTO `urls` (`id`, `user_id`, `long_url`, `short_url`, `clic_count`, `disabled`) VALUES
-(3, 1, 'https://www.songsterr.com/a/wsa/metallica-one-tab-s444t3', '49e30a4b', 0, 0);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
-(1, 'kk', '$2y$10$w5LnjS1tnTR8TIFRSXWqoeVIoKQwwLoPyfoTRWKXIwf.TUBhDhKIi', 'prout@gmail.com');
-
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `files`
---
-ALTER TABLE `files`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
-
---
--- Index pour la table `urls`
---
-ALTER TABLE `urls`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
-
---
--- Index pour la table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `files`
---
-ALTER TABLE `files`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `urls`
---
-ALTER TABLE `urls`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT pour la table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `files`
---
-ALTER TABLE `files`
-  ADD CONSTRAINT `files_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
-
---
--- Contraintes pour la table `urls`
---
-ALTER TABLE `urls`
-  ADD CONSTRAINT `urls_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Création de la table 'urls'
+CREATE TABLE IF NOT EXISTS urls (
+    id INT AUTO_INCREMENT,
+    user_id INT,
+    long_url TEXT,
+    short_url VARCHAR(255),
+    click_count INT DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    disabled BOOLEAN DEFAULT false,
+    link_type ENUM('url', 'file') DEFAULT 'url',
+    file_name VARCHAR(255),
+    display_name VARCHAR(255),
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
