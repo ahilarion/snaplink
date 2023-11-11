ee9d8295c4c546ffb905a96573bca322990ac4f1
Nelo07o <90777157+Nelo07o@users.noreply.github.com>
2023-11-11 14:54:50 +0100

    add final style

--- index.php
+++ index.php
@@ -67,6 +67,7 @@
   <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="icon" href="assets/svg/logo.svg">
   <link rel="stylesheet" href="dist/css/style.css">
   <script defer src="dist/js/script.js"></script>
   <title>SnapLink</title>
@@ -100,5 +101,8 @@ switch ($url) {
 }
 ?>
 
+<?php
+include 'components/footer.php'
+?>
 </body>
 </html>
