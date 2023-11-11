ce0bda628b01e185bba65b4ded5ea049b54c483d
Alan <alan.hilarion@proton.me>
2023-11-11 16:42:00 +0100

    Fix : header.php

--- components/header.php
+++ components/header.php
@@ -1,11 +1,18 @@
+<?php
+$currentPage = $_GET['pages'] ?? 'home';
+?>
+
 <header class="header grid wrapper">
-  <span class="header__logo"><a class="gradient-text" href="<?= BASE_URL; ?>index.php">SnapLink</a></span>
-  <div class="header__container-btn">
-    <?php if (isset($user) && $user->isLogged()): ?>
-      <button class="secondary-btn" onclick="window.location.href='<?= BASE_URL; ?>index.php?pages=logout'">Se Déconnecter</button>
-    <?php else: ?>
-        <button class="secondary-btn" onclick="window.location.href='<?= BASE_URL; ?>index.php?pages=login'">Se Connecter</button>
-        <button class="primary-btn" onclick="window.location.href='<?= BASE_URL; ?>index.php?pages=register'">S'inscrire</button>
-    <?php endif; ?>
-  </div>
+    <span class="header__logo"><a class="gradient-text" href="<?= BASE_URL; ?>index.php">SnapLink</a></span>
+    <div class="header__container-btn">
+        <?php if (isset($user) && $user->isLogged()): ?>
+            <button class="secondary-btn" onclick="window.location.href='<?= BASE_URL; ?>index.php?pages=logout'">Se Déconnecter</button>
+        <?php else: ?>
+            <?php if ($currentPage === 'login'): ?>
+                <button class="secondary-btn" onclick="window.location.href='<?= BASE_URL; ?>index.php?pages=register'">S'inscrire</button>
+            <?php else: ?>
+                <button class="secondary-btn" onclick="window.location.href='<?= BASE_URL; ?>index.php?pages=login'">Se Connecter</button>
+            <?php endif; ?>
+        <?php endif; ?>
+    </div>
 </header>
